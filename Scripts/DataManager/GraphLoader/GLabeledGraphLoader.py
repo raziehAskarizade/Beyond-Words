3d83b8cfd5839725f7cd1f72c8ecd947bdaeb60b
Fardin Rastakhiz <fardin.rastakhiz@gmail.com>
2023-10-25 16:19:32 +0330

    KGC-18 Update structure for dataloader

--- Scripts/DataManager/GraphLoader/GLabeledGraphLoader.py
+++ Scripts/DataManager/GraphLoader/GLabeledGraphLoader.py
@@ -1,4 +1,5 @@
 
+from Scripts.DataManager.GraphConstructor.GraphConstructor import GraphConstructor
 from Scripts.DataManager.GraphLoader.GraphLoader import GraphLoader
 import torch
 from sklearn.model_selection import train_test_split
@@ -6,21 +7,20 @@ from sklearn.model_selection import train_test_split
 
 class GLabeledGraphLoader(GraphLoader):
 
-    def __init__(self, graphs, graph_label, edge_index, batch_size, device,
+    def __init__(self, graph_constructor: GraphConstructor, graph_label, batch_size, device,
                  test_size=0.2, val_size=0.15, *args, **kwargs):
         self.device = device
         self.batch_size = batch_size
-        self.num_features = nodes_x.shape[1]
+        self.num_node_features = graph_constructor.get_first().shape[1]
         self.num_classes = len(torch.unique(graph_label))
         super(GLabeledGraphLoader, self)\
-            .__init__(  edge_index, device, test_size, val_size, *args, **kwargs)
+            .__init__(device, test_size, val_size, *args, **kwargs)
 
     def shuffle_train_test(self):
         self.train_indices, self.test_indices = train_test_split(
             torch.concat([self.train_indices, self.test_indices]), test_size=self._test_size)
 
     def get_train_data(self):
-
         return self.nodes[self.train_indices], self.node_labels[self.train_indices], self.edge_indices_train
 
     def get_test_data(self):
