a9d0486ecbdf1b5af61e8c4ec10fb2f4e9ed76fa
omidacoder <omiddavar@yahoo.com>
2023-10-26 19:53:30 +0330

    KGC-17 Sequential graph constructor done

--- Scripts/DataManager/GraphConstructor/SequentialGraphConstructor.py
+++ Scripts/DataManager/GraphConstructor/SequentialGraphConstructor.py
@@ -14,19 +14,20 @@ import numpy as np
 import os
 
 
-class TagsGraphConstructor(GraphConstructor):
+class SequentialGraphConstructor(GraphConstructor):
     
     class _Variables(GraphConstructor._Variables):
         def __init__(self):
-            super(TagsGraphConstructor._Variables, self).__init__()
+            super(SequentialGraphConstructor._Variables, self).__init__()
             self.nlp_pipeline: str = ''
     def __init__(self, texts: List[str], save_path: str, config: Config,
-                 lazy_construction=True, load_preprocessed_data=False, naming_prepend=''):
+                 lazy_construction=True, load_preprocessed_data=False, naming_prepend='', use_general_node=False):
 
-        super(TagsGraphConstructor, self)\
+        super(SequentialGraphConstructor, self)\
             .__init__(texts, self._Variables(), save_path, config, lazy_construction, load_preprocessed_data,
                       naming_prepend)
-        self.settings = {"tokens_tag_weight" : 1, "token_token_weight" : 2}
+        self.settings = {"tokens_general_weight" : 1, "token_token_weight" : 2 , "general_tokens_weight" : 2}
+        self.use_general_node = use_general_node
         if self.load_preprocessed_data:
             if not self.lazy_construction:
                 self.load_all_data()
@@ -36,7 +37,6 @@ class TagsGraphConstructor(GraphConstructor):
             self.var.nlp_pipeline = self.config.spacy.pipeline
             self.var.graph_num = len(self.raw_data)
             self.nlp = spacy.load(self.var.nlp_pipeline)
-            self.tags = self.nlp.get_pipe("tagger").labels
             if not self.lazy_construction:
                 for i in range(len(self.raw_data)):
                     if i not in self._graphs:
@@ -51,14 +51,11 @@ class TagsGraphConstructor(GraphConstructor):
         if len(doc) < 2:
             return
         return self.__create_graph(doc)
-    def __find_tag_index(self , tag : str):
-        for tag_idx in range(len(self.tags)):
-            if self.tags[tag_idx] == tag:
-                return tag_idx
-        return -1 # means not found
     def __create_graph(self , doc):
-        tags_length = len(self.tags)
-        node_attr = torch.zeros((len(doc) + tags_length, self.nlp.vocab.vectors_length), dtype=torch.float32)
+        docs_length = len(doc)
+        if self.use_general_node:
+            docs_length += 1
+        node_attr = torch.zeros((docs_length, self.nlp.vocab.vectors_length), dtype=torch.float32)
         edge_index = []
         edge_attr = []
         
@@ -68,31 +65,39 @@ class TagsGraphConstructor(GraphConstructor):
         for token in doc:
             token_id = self.nlp.vocab.strings[token.lemma_]
             if token_id in self.nlp.vocab.vectors:
-                node_attr[token.i + tags_length - 1] = torch.tensor(self.nlp.vocab.vectors[token_id])
-            tag_idx = self.__find_tag_index(token.tag_)
-            if tag_idx != -1:
-                edge_index.append([token.i + tags_length - 1 , tag_idx])
-                edge_attr.append(self.settings["tokens_tag_weight"])
-                edge_index.append([tag_idx , token.i + tags_length - 1])
-                edge_attr.append(self.settings["tokens_tag_weight"])
+                if self.use_general_node:
+                    node_attr[token.i + 1] = torch.tensor(self.nlp.vocab.vectors[token_id])
+                else:
+                    node_attr[token.i] = torch.tensor(self.nlp.vocab.vectors[token_id])
+            if self.use_general_node:
+                edge_index.append([token.i + 1 , 0])
+                edge_attr.append(self.settings['tokens_general_weight'])
+                edge_index.append([0 , token.i + 1])
+                edge_attr.append(self.settings['general_tokens_weight'])
             # adding sequential edges between tokens - uncomment the codes for vectorized edges
             if token.i != len(doc) - 1:
                 # using zero vectors for edge features
-                edge_index.append([token.i + tags_length - 1 , token.i + 1])
-                # self.edge_attr.append(torch.zeros((self.nlp.vocab.vectors_length,), dtype=torch.float32))
-                edge_attr.append(self.settings["token_token_weight"])
-                edge_index.append([token.i + 1, token.i + tags_length - 1])
-                # self.edge_attr.append(torch.zeros((self.nlp.vocab.vectors_length,), dtype=torch.float32))
-                edge_attr.append(self.settings["token_token_weight"])
+                if self.use_general_node:
+                    edge_index.append([token.i + 1 , token.i + 2])
+                    edge_index.append([token.i + 2 , token.i + 1])
+                    # self.edge_attr.append(torch.zeros((self.nlp.vocab.vectors_length,), dtype=torch.float32))
+                    edge_attr.append(self.settings["token_token_weight"]) 
+                    edge_attr.append(self.settings["token_token_weight"]) 
+                else:
+                    edge_index.append([token.i , token.i + 1])
+                    edge_index.append([token.i + 1 , token.i])
+                    # self.edge_attr.append(torch.zeros((self.nlp.vocab.vectors_length,), dtype=torch.float32))
+                    edge_attr.append(self.settings["token_token_weight"]) 
+                    edge_attr.append(self.settings["token_token_weight"]) 
         # self.node_attr = node_attr
         edge_index = torch.transpose(torch.tensor(edge_index, dtype=torch.long) , 0 , 1)
         # self.edge_attr = edge_attr # vectorized edge attributes
         return Data(x=node_attr, edge_index=edge_index,edge_attr=edge_attr)
     def draw_graph(self , idx : int):
         node_tokens = []
+        if self.use_general_node:
+            node_tokens.append("gen_node")
         doc = self.nlp(self.raw_data[idx])
-        for d in self.tags:
-            node_tokens.append(d)
         for t in doc:
             node_tokens.append(t.lemma_)
         graph_data = self.get_graph(idx)
