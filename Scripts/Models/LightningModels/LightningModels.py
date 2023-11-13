67adb29cdb0be346bca54af79b0f20a98cf17c51
Fardin Rastakhiz <fardin.rastakhiz@gmail.com>
2023-11-13 16:40:56 +0330

    KGC-73 Test and improve models, Add new loss function

--- Scripts/Models/LightningModels/LightningModels.py
+++ Scripts/Models/LightningModels/LightningModels.py
@@ -5,7 +5,6 @@ from pytorch_lightning.utilities.types import OptimizerLRScheduler
 import lightning as L
 from abc import abstractmethod
 
-
 class BaseLightningModel(L.LightningModule):
 
     def __init__(self, model, optimizer=None, loss_func=None, learning_rate=0.01, batch_size=64, lr_scheduler=None, user_lr_scheduler=False):
@@ -26,7 +25,7 @@ class BaseLightningModel(L.LightningModule):
         param_groups = next(iter(self.optimizer.param_groups))
         if 'lr' in param_groups and param_groups['lr'] is not None:
             current_learning_rate = float(param_groups['lr'])
-            self.log('lr', current_learning_rate, batch_size=self.batch_size, on_epoch=True, on_step=False)
+            self.log('lr', current_learning_rate, prog_bar=True, batch_size=self.batch_size, on_epoch=True, on_step=False)
     
     def training_step(self, data_batch, *args, **kwargs):
         data, labels = data_batch
@@ -34,7 +33,7 @@ class BaseLightningModel(L.LightningModule):
         labels = labels.to(self.device)
         out_features = self(data)
         loss = self.loss_func(out_features, labels.view(out_features.shape))
-        self.log('train_loss', loss, batch_size=self.batch_size, on_epoch=True, on_step=False)
+        self.log('train_loss', loss, batch_size=self.batch_size, prog_bar=True, on_epoch=True, on_step=False)
         return loss, out_features
 
     def validation_step(self, data_batch, *args, **kwargs):
@@ -93,7 +92,6 @@ class BaseLightningModel(L.LightningModule):
     def _get_loss_func(self, loss_func):
         pass
 
-
 class BinaryLightningModel(BaseLightningModel):
 
     def __init__(self, model, optimizer=None, loss_func=None, learning_rate=0.01, batch_size=64, lr_scheduler=None, user_lr_scheduler=False):
@@ -119,7 +117,6 @@ class BinaryLightningModel(BaseLightningModel):
         return loss_func \
             if loss_func is not None else \
             torch.nn.BCELoss()
-
 
 class MultiClassLightningModel(BaseLightningModel):
 
@@ -147,7 +144,6 @@ class MultiClassLightningModel(BaseLightningModel):
             if loss_func is not None else \
             torch.nn.CrossEntropyLoss()
 
-
 class MultiLabelLightningModel(BaseLightningModel):
 
     def __init__(self, model, optimizer=None, loss_func=None, lr=0.01, batch_size=64, user_lr_scheduler=False):
@@ -173,3 +169,45 @@ class MultiLabelLightningModel(BaseLightningModel):
         return loss_func \
             if loss_func is not None else \
             torch.nn.CrossEntropyLoss()
+
+
+# class HeteroBinaryLightningModel(BaseLightningModel):
+
+#     def __init__(self, model, optimizer=None, loss_func=None, learning_rate=0.01, batch_size=64, lr_scheduler=None, user_lr_scheduler=False):
+#         super(HeteroBinaryLightningModel, self).__init__(model, optimizer, loss_func, learning_rate, batch_size=batch_size, lr_scheduler=lr_scheduler, user_lr_scheduler=user_lr_scheduler)
+#         self.train_acc = torchmetrics.Accuracy(task="binary")
+#         self.val_acc = torchmetrics.Accuracy(task="binary")
+#         self.test_acc = torchmetrics.Accuracy(task="binary")
+
+#     def training_step(self, data_batch, *args, **kwargs):    
+#         data, labels = data_batch
+#         data = data.to(self.device)
+#         labels = labels.to(self.device)
+#         out_features = self(data)
+#         h_out_features = HeteroLossArgs(out_features[0], out_features[1])
+#         label_features = HeteroLossArgs(labels.view(out_features[0].shape), data.x_dict)
+#         loss = self.loss_func(h_out_features, label_features)
+#         self.log('train_loss', loss, batch_size=self.batch_size, on_epoch=True, on_step=False)
+        
+#         predicted_labels = out_features[0] if out_features[0].shape[1] < 2 else torch.argmax(out_features[0], dim=1)
+#         self.train_acc(predicted_labels, data_batch[1].view(predicted_labels.shape))
+#         self.log('train_acc', self.train_acc, prog_bar=True, on_epoch=True, on_step=True, batch_size=self.batch_size)
+#         return loss
+
+#     def validation_step(self, data_batch, *args, **kwargs):
+#         data, labels = data_batch
+#         data = data.to(self.device)
+#         labels = labels.to(self.device)
+#         out_features = self(data)
+#         h_out_features = HeteroLossArgs(out_features[0], out_features[1])
+#         label_features = HeteroLossArgs(labels.view(out_features[0].shape), data.x_dict)
+#         loss = self.loss_func(h_out_features, label_features)
+#         self.log('val_loss', loss, batch_size=self.batch_size, on_epoch=True, on_step=False)
+#         predicted_labels = out_features[0] if out_features[0].shape[1] < 2 else torch.argmax(out_features[0], dim=1)
+#         self.val_acc(predicted_labels, data_batch[1].view(predicted_labels.shape))
+#         self.log('val_acc', self.val_acc, prog_bar=True, on_epoch=True, on_step=True, batch_size=self.batch_size)
+
+#     def _get_loss_func(self, loss_func):
+#         return loss_func \
+#             if loss_func is not None else \
+#             torch.nn.BCELoss()
