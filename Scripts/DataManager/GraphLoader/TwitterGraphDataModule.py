9b0bd88a714737c66df1b801e5fc874af886902b
omidacoder <omiddavar@yahoo.com>
2023-11-20 18:18:43 +0330

    KGC-100 twitter bugs fixed

--- Scripts/DataManager/GraphLoader/TwitterGraphDataModule.py
+++ Scripts/DataManager/GraphLoader/TwitterGraphDataModule.py
@@ -39,7 +39,7 @@ class TwitterGraphDataModule(GraphDataModule):
         self.drop_last = drop_last
         self.graph_type = graph_type
         self.graphs_path = graphs_path if graphs_path != '' else 'data/GraphData/Sentiment140'
-        self.data_path = 'data/Sentiment140/sentiment140.csv' if data_path == '' else data_path
+        self.data_path = 'data/Sentiment140/sentiment140-balanced.csv' if data_path == '' else data_path
         self.labels = None
         self.dataset = None
         self.shuffle = shuffle
@@ -51,19 +51,19 @@ class TwitterGraphDataModule(GraphDataModule):
         self.df = pd.read_csv(
             path.join(self.config.root, self.data_path) , encoding='latin-1')
 
-        self.df.columns = ['Polarity', 'Time', 'Date', 'Query', 'User' , 'Tweet']
         self.df = self.df[['Polarity', 'Tweet']]
         self.end_data_load = self.end_data_load if self.end_data_load > 0 else self.df.shape[0]
         self.end_data_load = self.end_data_load if self.end_data_load < self.df.shape[0] else self.df.shape[0]
         # removing tweets less than 3 words
-        self.df = self.df[self.df['Tweet'].str.split().str.len().gt(2)]  
+        # self.df = self.df[self.df['Tweet'].str.split().str.len().gt(2)]  
         # balancing the dataset
-        g = self.df.groupby('Polarity')
-        self.df = g.apply(lambda x: x.sample(self.end_data_load if self.end_data_load > 0 else g.size().min()).reset_index(drop=True))
-        self.df = self.df.sample(frac=1 , random_state=1).reset_index(drop=True) # using seed in shuffling
+        # g = self.df.groupby('Polarity')
+        # self.df = g.apply(lambda x: x.sample(self.end_data_load if self.end_data_load > 0 else g.size().min()).reset_index(drop=True))
+        # self.df = self.df.sample(frac=1 , random_state=1).reset_index(drop=True) # using seed in shuffling
     
-        self.df = self.df.iloc[: self.end_data_load]
-        self.df.index = np.arange(0, self.end_data_load)
+        self.df = self.df.iloc[:self.end_data_load]
+        # self.df.index = np.arange(0, self.end_data_load)
+        # self.df.to_csv('sentiment140-balanced.csv' , index=False)
         # activate one line below
         labels = self.df['Polarity'][self.start_data_load:self.end_data_load]
         labels = labels.apply(lambda p: 0 if p == 0 else 1).to_numpy()
