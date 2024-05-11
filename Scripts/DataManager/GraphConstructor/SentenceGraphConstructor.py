d00bdbb1d5e119fc56337f24409528993999b5e8
raziehAskarizade <raziehaskarizade@gmail.com>
2024-05-11 13:40:26 +0330

    replace token.i with i

--- Scripts/DataManager/GraphConstructor/SentenceGraphConstructor.py
+++ Scripts/DataManager/GraphConstructor/SentenceGraphConstructor.py
@@ -8,7 +8,7 @@ import pandas as pd
 from torch_geometric.utils import to_networkx
 
 from Scripts.DataManager.GraphConstructor.SequentialGraphConstructor import SequentialGraphConstructor
-from torch_geometric.data import Data , HeteroData
+from torch_geometric.data import Data, HeteroData
 from Scripts.Configs.ConfigClass import Config
 
 import torch
@@ -21,27 +21,28 @@ import stanza
 
 
 class SentenceGraphConstructor(SequentialGraphConstructor):
-    
+
     class _Variables(SequentialGraphConstructor._Variables):
         def __init__(self):
             super(SentenceGraphConstructor._Variables, self).__init__()
             self.nlp_pipeline: str = ''
-            
+
     def __init__(self, texts: List[str], save_path: str, config: Config,
-                load_preprocessed_data=False, naming_prepend='', use_general_node=False , use_compression=True, start_data_load=0, end_data_load=-1 , num_general_nodes=1):
+                 load_preprocessed_data=False, naming_prepend='', use_general_node=False, use_compression=True, start_data_load=0, end_data_load=-1, num_general_nodes=1):
 
         super(SentenceGraphConstructor, self)\
             .__init__(texts, save_path, config, load_preprocessed_data,
-                      naming_prepend , False , use_compression,start_data_load, end_data_load,num_general_nodes)
-        self.settings = {"token_sentence_weight" : 1, "token_token_weight" : 2 , "general_sentence_weight" : 2}
+                      naming_prepend, False, use_compression, start_data_load, end_data_load, num_general_nodes)
+        self.settings = {"token_sentence_weight": 1,
+                         "token_token_weight": 2, "general_sentence_weight": 2}
         self.use_general_node = use_general_node
         self.var.nlp_pipeline = self.config.fa.pipeline
         self.var.graph_num = len(self.raw_data)
-        
+
         # farsi
         self.nlp = fasttext.load_model(self.var.nlp_pipeline)
         self.token_lemma = stanza.Pipeline("fa")
-        
+
     def to_graph(self, text: str):
 
         # farsi
@@ -49,24 +50,25 @@ class SentenceGraphConstructor(SequentialGraphConstructor):
         token_list = self.token_lemma(text)
         for sentence in token_list.sentences:
             for token in sentence.words:
-                doc.append((token.text,token.lemma))
+                doc.append((token.text, token.lemma))
 
         if len(doc) < 2:
             return
         return self.__create_sentence_graph(doc)
-    
-    def __create_sentence_graph(self , doc , for_compression=False):
-        sequential_data = super()._create_graph(doc , for_compression) # homogeneous
+
+    def __create_sentence_graph(self, doc, for_compression=False):
+        sequential_data = super()._create_graph(doc, for_compression)  # homogeneous
         data = HeteroData()
         sentence_embeddings = [sent.vector for sent in doc.sents]
         data['word'].x = sequential_data.x
         if for_compression:
             if self.use_general_node:
-                data['general'].x = torch.full((1,),-1, dtype=torch.float32)
+                data['general'].x = torch.full((1,), -1, dtype=torch.float32)
         else:
             if self.use_general_node:
                 data['general'].x = self._build_initial_general_vector()
-        data['sentence'].x = torch.tensor(sentence_embeddings, dtype=torch.float32)
+        data['sentence'].x = torch.tensor(
+            sentence_embeddings, dtype=torch.float32)
         sentence_general_edge_index = []
         general_sentence_edge_index = []
         sentence_word_edge_index = []
@@ -76,53 +78,65 @@ class SentenceGraphConstructor(SequentialGraphConstructor):
         sentence_word_edge_attr = []
         word_sentence_edge_attr = []
         if self.use_general_node:
-            for i , _x in enumerate(doc.sents):
+            for i, _x in enumerate(doc.sents):
                 # connecting sentences to general node
-                sentence_general_edge_index.append([i , 0])
-                general_sentence_edge_index.append([0 , i])
-                sentence_general_edge_attr.append(self.settings['general_sentence_weight'])
-                general_sentence_edge_attr.append(self.settings['general_sentence_weight']) # different weight for directed edges can be set in the future
+                sentence_general_edge_index.append([i, 0])
+                general_sentence_edge_index.append([0, i])
+                sentence_general_edge_attr.append(
+                    self.settings['general_sentence_weight'])
+                # different weight for directed edges can be set in the future
+                general_sentence_edge_attr.append(
+                    self.settings['general_sentence_weight'])
         sent_index = -1
-        for token in doc:
+        for i, token in enumerate(doc):
             # connecting words to sentences
             if token.is_sent_start:
                 sent_index += 1
-            word_sentence_edge_index.append([token.i, sent_index])
-            sentence_word_edge_index.append([sent_index, token.i])
-            word_sentence_edge_attr.append(self.settings['token_sentence_weight'])
-            sentence_word_edge_attr.append(self.settings['token_sentence_weight'])
+            word_sentence_edge_index.append([i, sent_index])
+            sentence_word_edge_index.append([sent_index, i])
+            word_sentence_edge_attr.append(
+                self.settings['token_sentence_weight'])
+            sentence_word_edge_attr.append(
+                self.settings['token_sentence_weight'])
         if self.use_general_node:
-            data['general' , 'general_sentence' , 'sentence'].edge_index = torch.transpose(torch.tensor(general_sentence_edge_index, dtype=torch.int32) , 0 , 1) if len(general_sentence_edge_index) > 0 else torch.empty(2, 0, dtype=torch.int32)
-            data['sentence' , 'sentence_general' , 'general'].edge_index = torch.transpose(torch.tensor(sentence_general_edge_index, dtype=torch.int32) , 0 , 1) if len(sentence_general_edge_index) > 0 else torch.empty(2, 0, dtype=torch.int32)
-            data['general' , 'general_sentence' , 'sentence'].edge_attr = torch.tensor(general_sentence_edge_attr, dtype=torch.float32)
-            data['sentence' , 'sentence_general' , 'general'].edge_attr = torch.tensor(sentence_general_edge_attr, dtype=torch.float32)
-        data['word' , 'word_sentence' , 'sentence'].edge_index = torch.transpose(torch.tensor(word_sentence_edge_index, dtype=torch.int32) , 0 , 1) if len(word_sentence_edge_index) > 0 else torch.empty(2, 0, dtype=torch.int32)
-        data['sentence' , 'sentence_word' , 'word'].edge_index = torch.transpose(torch.tensor(sentence_word_edge_index, dtype=torch.int32) , 0 , 1) if len(sentence_word_edge_index) > 0 else torch.empty(2, 0, dtype=torch.int32)
-        data['word' , 'seq' , 'word'].edge_index = sequential_data.edge_index
-        data['word' , 'word_sentence' , 'sentence'].edge_attr = torch.tensor(word_sentence_edge_attr, dtype=torch.float32)
-        data['sentence' , 'sentence_word' , 'word'].edge_attr = torch.tensor(sentence_word_edge_attr, dtype=torch.float32)
-        data['word' , 'seq' , 'word'].edge_attr = sequential_data.edge_attr
+            data['general', 'general_sentence', 'sentence'].edge_index = torch.transpose(torch.tensor(
+                general_sentence_edge_index, dtype=torch.int32), 0, 1) if len(general_sentence_edge_index) > 0 else torch.empty(2, 0, dtype=torch.int32)
+            data['sentence', 'sentence_general', 'general'].edge_index = torch.transpose(torch.tensor(
+                sentence_general_edge_index, dtype=torch.int32), 0, 1) if len(sentence_general_edge_index) > 0 else torch.empty(2, 0, dtype=torch.int32)
+            data['general', 'general_sentence', 'sentence'].edge_attr = torch.tensor(
+                general_sentence_edge_attr, dtype=torch.float32)
+            data['sentence', 'sentence_general', 'general'].edge_attr = torch.tensor(
+                sentence_general_edge_attr, dtype=torch.float32)
+        data['word', 'word_sentence', 'sentence'].edge_index = torch.transpose(torch.tensor(
+            word_sentence_edge_index, dtype=torch.int32), 0, 1) if len(word_sentence_edge_index) > 0 else torch.empty(2, 0, dtype=torch.int32)
+        data['sentence', 'sentence_word', 'word'].edge_index = torch.transpose(torch.tensor(
+            sentence_word_edge_index, dtype=torch.int32), 0, 1) if len(sentence_word_edge_index) > 0 else torch.empty(2, 0, dtype=torch.int32)
+        data['word', 'seq', 'word'].edge_index = sequential_data.edge_index
+        data['word', 'word_sentence', 'sentence'].edge_attr = torch.tensor(
+            word_sentence_edge_attr, dtype=torch.float32)
+        data['sentence', 'sentence_word', 'word'].edge_attr = torch.tensor(
+            sentence_word_edge_attr, dtype=torch.float32)
+        data['word', 'seq', 'word'].edge_attr = sequential_data.edge_attr
         return data
-                    
+
     def to_graph_indexed(self, text: str):
         doc = self.nlp(text)
         if len(doc) < 2:
             return
-        return self.__create_sentence_graph(doc , for_compression=True)
-    
+        return self.__create_sentence_graph(doc, for_compression=True)
+
     def prepare_loaded_data(self, graph):
-        words = torch.zeros((len(graph['word'].x) , self.nlp.vocab.vectors_length), dtype=torch.float32)
+        words = torch.zeros(
+            (len(graph['word'].x), self.nlp.vocab.vectors_length), dtype=torch.float32)
         for i in range(len(graph['word'].x)):
             if graph['word'].x[i] in self.nlp.vocab.vectors:
-                words[i] = torch.tensor(self.nlp.vocab.vectors[graph['word'].x[i]])
+                words[i] = torch.tensor(
+                    self.nlp.vocab.vectors[graph['word'].x[i]])
         graph['word'].x = words
         if self.use_general_node:
-            graph = self._add_multiple_general_nodes(graph , True , self.num_general_nodes)
+            graph = self._add_multiple_general_nodes(
+                graph, True, self.num_general_nodes)
         for t in graph.edge_types:
             if len(graph[t].edge_index) == 0:
                 graph[i].edge_index = torch.empty(2, 0, dtype=torch.int32)
         return graph
-    
-
-        
-
--- Scripts/DataManager/GraphConstructor/SentimentGraphConstructor.py
+++ Scripts/DataManager/GraphConstructor/SentimentGraphConstructor.py
@@ -8,7 +8,7 @@ import pandas as pd
 from torch_geometric.utils import to_networkx
 
 from Scripts.DataManager.GraphConstructor.TagDepTokenGraphConstructor import TagDepTokenGraphConstructor
-from torch_geometric.data import Data , HeteroData
+from torch_geometric.data import Data, HeteroData
 from Scripts.Configs.ConfigClass import Config
 import spacy
 import torch
@@ -18,74 +18,81 @@ from spacytextblob.spacytextblob import SpacyTextBlob
 
 
 class SentimentGraphConstructor(TagDepTokenGraphConstructor):
-    
+
     class _Variables(TagDepTokenGraphConstructor._Variables):
         def __init__(self):
             super(SentimentGraphConstructor._Variables, self).__init__()
             self.nlp_pipeline: str = ''
-            
+
     def __init__(self, texts: List[str], save_path: str, config: Config,
-                load_preprocessed_data=False, naming_prepend='' , use_compression=True,use_sentence_nodes=False, use_general_node=True, start_data_load=0, end_data_load=-1,num_general_nodes = 1):
+                 load_preprocessed_data=False, naming_prepend='', use_compression=True, use_sentence_nodes=False, use_general_node=True, start_data_load=0, end_data_load=-1, num_general_nodes=1):
 
         super(SentimentGraphConstructor, self)\
             .__init__(texts, save_path, config, load_preprocessed_data,
-                      naming_prepend  , use_compression,use_sentence_nodes, use_general_node, start_data_load, end_data_load,num_general_nodes)
+                      naming_prepend, use_compression, use_sentence_nodes, use_general_node, start_data_load, end_data_load, num_general_nodes)
         # self.settings["token_sentiment_weight"] = 2
         self.nlp.add_pipe('spacytextblob')
-        
+
     def to_graph(self, text: str):
         doc = self.nlp(text)
         # if len(doc) < 2:
         #     return
         return self.__create_sentiment_graph(doc)
+
     def _build_initial_sentiment_vector(self):
-        return torch.zeros((2 , self.nlp.vocab.vectors_length), dtype=torch.float32)   
-    
-    def __create_sentiment_graph(self , doc , for_compression=False):
+        return torch.zeros((2, self.nlp.vocab.vectors_length), dtype=torch.float32)
+
+    def __create_sentiment_graph(self, doc, for_compression=False):
         if for_compression:
             data = super().to_graph_indexed(doc)
         else:
             data = super().to_graph(doc)
         # adding sentiment nodes
         if for_compression:
-            data['sentiment'].x = torch.full((2,),-1, dtype=torch.float32)
+            data['sentiment'].x = torch.full((2,), -1, dtype=torch.float32)
         else:
             data['sentiment'].x = self._build_initial_sentiment_vector()
         sentiment_word_edge_index = []
         word_sentiment_edge_index = []
         sentiment_word_edge_attr = []
         word_sentiment_edge_attr = []
-        for token in doc:
+        for i, token in enumerate(doc):
             if token._.blob.polarity > 0:
-                word_sentiment_edge_index.append([token.i, 1])
-                sentiment_word_edge_index.append([1, token.i])
+                word_sentiment_edge_index.append([i, 1])
+                sentiment_word_edge_index.append([1, i])
                 word_sentiment_edge_attr.append(abs(token._.blob.polarity))
                 sentiment_word_edge_attr.append(abs(token._.blob.polarity))
             if token._.blob.polarity < 0:
-                word_sentiment_edge_index.append([token.i, 0])
-                sentiment_word_edge_index.append([0, token.i])
+                word_sentiment_edge_index.append([i, 0])
+                sentiment_word_edge_index.append([0, i])
                 word_sentiment_edge_attr.append(abs(token._.blob.polarity))
                 sentiment_word_edge_attr.append(abs(token._.blob.polarity))
-        data['word' , 'word_sentiment' , 'sentiment'].edge_index = torch.transpose(torch.tensor(word_sentiment_edge_index, dtype=torch.int32) , 0 , 1) if len(word_sentiment_edge_index) > 0 else torch.empty(2, 0, dtype=torch.int32)
-        data['sentiment' , 'sentiment_word' , 'word'].edge_index = torch.transpose(torch.tensor(sentiment_word_edge_index, dtype=torch.int32) , 0 , 1) if len(sentiment_word_edge_index) > 0 else torch.empty(2, 0, dtype=torch.int32)
-        data['word' , 'word_sentiment' , 'sentiment'].edge_attr = torch.tensor(word_sentiment_edge_attr, dtype=torch.float32)
-        data['sentiment' , 'sentiment_word' , 'word'].edge_attr = torch.tensor(sentiment_word_edge_attr, dtype=torch.float32)
+        data['word', 'word_sentiment', 'sentiment'].edge_index = torch.transpose(torch.tensor(
+            word_sentiment_edge_index, dtype=torch.int32), 0, 1) if len(word_sentiment_edge_index) > 0 else torch.empty(2, 0, dtype=torch.int32)
+        data['sentiment', 'sentiment_word', 'word'].edge_index = torch.transpose(torch.tensor(
+            sentiment_word_edge_index, dtype=torch.int32), 0, 1) if len(sentiment_word_edge_index) > 0 else torch.empty(2, 0, dtype=torch.int32)
+        data['word', 'word_sentiment', 'sentiment'].edge_attr = torch.tensor(
+            word_sentiment_edge_attr, dtype=torch.float32)
+        data['sentiment', 'sentiment_word', 'word'].edge_attr = torch.tensor(
+            sentiment_word_edge_attr, dtype=torch.float32)
         return data
-                    
+
     def to_graph_indexed(self, text: str):
         doc = self.nlp(text)
         # if len(doc) < 2:
         #     return
-        return self.__create_sentiment_graph(doc , for_compression=True)
-    
+        return self.__create_sentiment_graph(doc, for_compression=True)
+
     def prepare_loaded_data(self, graph):
-        graph = super(SentimentGraphConstructor , self).prepare_loaded_data(graph)
+        graph = super(SentimentGraphConstructor,
+                      self).prepare_loaded_data(graph)
         graph['sentiment'].x = self._build_initial_sentiment_vector()
         for t in graph.edge_types:
             if graph[t].edge_index.shape[1] == 0:
                 graph[t].edge_index = torch.empty(2, 0, dtype=torch.int32)
         return graph
-    def remove_node_type_from_graphs(self,node_name : str):
+
+    def remove_node_type_from_graphs(self, node_name: str):
         for i in range(len(self._graphs)):
             if self._graphs[i] is not None:
                 if node_name in self._graphs[i].node_types:
@@ -93,8 +100,3 @@ class SentimentGraphConstructor(TagDepTokenGraphConstructor):
                 for edge_type in self._graphs[i].edge_types:
                     if edge_type[0] == node_name or edge_type[1] == node_name:
                         del self._graphs[i][edge_type]
-        
-    
-
-        
-
